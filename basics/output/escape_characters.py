cb361ce950f8cc3a76af34ead4a0340d47fb3bce
repl.it user <replituser@example.com>
2021-02-26 11:26:21 +0000

    escape characters

---
+++ basics/output/escape_characters.py
@@ -0,0 +1,6 @@
+#Display escape characters
+print("\n display new line")
+print("\t display a tab space")
+print("\\ displays a backslash")
+print("\" displays double quote")
+print("\' displays single quote")
