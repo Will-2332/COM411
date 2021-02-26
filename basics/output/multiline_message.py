71172dccf77a0d4c27fca4cda66e39609501b972
repl.it user <replituser@example.com>
2021-02-26 11:11:35 +0000

    multiline added

---
+++ basics/output/multiline_message.py
@@ -0,0 +1,7 @@
+#Display message in standard output to multiple lines
+print("System imminent failure!")
+print("system rebot initiated\n")
+print("... rebooting sensory system")
+print("... rebooting motors system")
+print("... rebooting hoover system\n")
+print("system online")
