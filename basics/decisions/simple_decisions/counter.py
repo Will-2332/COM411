1080bd4f20c5bebcaca9fb8f896547a4e2dd4de8
Willian Caio de Oliveira Caio <willian.musquim@gmail.com>
2021-03-04 14:19:14 +0000

    counter working

--- basics/decisions/simple_decisions/counter.py
+++ basics/decisions/simple_decisions/counter.py
@@ -1,18 +1,18 @@
 #program to count how many odds or even numbers there is
 even=0
 odds=0
-n1 = int(input("insert first number :"))
+n1 = float(input("insert first number :"))
 if (n1 % 2 == 0) :
   even= even + 1
 else :
   odds = odds + 1
-n2 = int(input("insert second number :"))
+n2 = float(input("insert second number :"))
 if (n2 % 2 == 0) :
   even = even + 1
 else :
   odds = odds + 1
-n3 = int(input("insert third number :"))
-if (n2 % 2 == 0) :
+n3 = float(input("insert third number :"))
+if (n3 % 2 == 0) :
   even = even + 1
 else :
   odds = odds + 1
