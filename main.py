399cdcf122fcd304e1fc7f3208c7c79505117dc6
repl.it user <replituser@example.com>
2021-03-21 15:09:03 +0000

    finished basics

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-print("Please enter a word")
-user_word = input()
 
-for position in range(0, len(user_word), 1):
-    print(user_word[position])
+def run_block_a():
+    print("Which program in 'Block A: Basics' do you wish to run?")
+    response = input()
+    if (response == "simple_message"):
+        import basics.output.simple_message
+    elif (response == "multiline_message"):
+        import basics.output.simple_message
+
+
+def run():
+    is_running = True
+
+    while(is_running):
+        print("What would you like to do?")
+        print("[a] Run 'Block A: Basics' programs")
+        print("[q] Quit")
+        response = input()
+
+        if (response == "a"):
+            run_block_a()
+        elif (response == "q"):
+            break
+        else:
+            print("Invalid option! Please try again.")
+
+run()
