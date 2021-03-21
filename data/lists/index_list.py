bba644b645d87734c48ca3762d55190e43419be7
Willian Musquim <67543404+Will-2332@users.noreply.github.com>
2021-03-21 15:21:24 +0000

    index list done

--- data/lists/index_list.py
+++ data/lists/index_list.py
@@ -1,6 +1,19 @@
 def movements():
   path=[]
-  path.append("Move Forward", 10, "Move Backward", 5, "Move Left", 3, "Move Right", 1)
+  path.append("Move Forward")
+  path.append(10)
+  path.append("Move Backward")
+  path.append(5)
+  path.append("Move Left")
+  path.append(3)
+  path.append("Move Right")
+  path.append(1)
   return path
 def run():
-  
+  print("Mooooving")
+  path = movements()
+  print(f"{path[0]} for {path[1]} steps")
+  print(f"{path[2]} for {path[3]} steps")
+  print(f"{path[4]} for {path[5]} steps")
+
+run()
