b0ce002bd40eca41ad30a2d5afde4370b4f12550
repl.it user <replituser@example.com>
2021-05-07 16:14:30 +0000

    lets see on pycharm

---
+++ visual/plots/dict.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import random as rnd
+
+def data():
+  paths={}
+  line_style=input("Which kind of line style would you like ? -- or - ?")
+  colour_style=input("Which kind of colour would you like ? r,g or b ?")
+  marker_style=input("Which kind of marker style would you like ? o, s or ^ ?")
+  paths['line_style'] = line_style
+  paths['colour'] = colour
+  paths['marker_style'] = marker_style
+  return paths
+
+def generate():
+  n=int(input("how many likes would you like to plot?"))
+  for n in range(n):
+    values=data()
+    x = rnd.sample(range(1, 100), 5)
+    y = rnd.sample(range(1, 100), 5)
+    format = f"{values['colour']}{values['line_style']}{values['marker_style']}"
+    plt.plot(x, y, format)
+
+  plt.show
+
+def run():
+  print("running")
+  generate()
+  print("done")
