4241638cc28fc6522551ad8dc8d0a28063dde549
Willian Caio de Oliveira Caio <>
2021-06-01 12:59:49 +0000

    a

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-
-def run_block_a():
-    print("Which program in 'Block A: Basics' do you wish to run?")
-    response = input()
-    if (response == "simple_message"):
-        import basics.output.simple_message
-    elif (response == "multiline_message"):
-        import basics.output.simple_message
-
-
-def run():
-    is_running = True
-
-    while(is_running):
-        print("What would you like to do?")
-        print("[a] Run 'Block A: Basics' programs")
-        print("[q] Quit")
-        response = input()
-
-        if (response == "a"):
-            run_block_a()
-        elif (response == "q"):
-            break
-        else:
-            print("Invalid option! Please try again.")
-
-run()
+def gang():
+    print("Loading gang...")
+    friends=[]
+    friends.append("Scooby Doo")
+    friends.append("Shaggy Rogers")
+    friends.append("Fred Jones")
+    friends.append("Daphne Blake")
+    friends.append("Velma Dinkley")
+    for i in range (len(friends)):
+        print(friends[i])
+    print("...Done!")
+    return friends
+    
+def phrases(friends):
+    quotes={}
+    for i in range (len(friends)):
+        print("What does",friends[i],"say?")
+        quote=input()
+        quotes[friends[i]] = quote
+    return quotes
+    
+def save(quotes):
+    with open("quotes.txt", "wb") as text:
+        text.write(str(quotes))
+        
+save(phrases(gang()))
